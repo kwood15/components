22e76a2020f43a5e5c329f051842534d444413dd
Kay Wood <kwood15@users.noreply.github.com>
2021-07-06 17:01:05 +0100

    Css module setup

--- webpack.common.js
+++ webpack.common.js
@@ -19,34 +19,68 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader, 'css-loader'
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: true,
-              // localIdentName: "[name]__[local]___[hash:base64:5]"
-            }
-          }
-        ]
+        use: ['style-loader', 'css-loader']
       }
+      // {
+      //   test: /\.css$/,
+      //   loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]'
+      // }
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     'style-loader',
+      //     {
+      //       loader: 'css-loader',
+      //       options: {
+      //         import: false,
+      //         modules: true
+      //       }
+      //     }
+      //   ],
+      //   include: /\.module\.css$/
+      // },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader'
+      //   ],
+      //   exclude: /\.module\.css$/
+      // }
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader'],
+      //   loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]'
+      // }
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     {
+      //       loader: MiniCssExtractPlugin.loader
+      //     },
+      //     {
+      //       loader: 'css-loader',
+      //       options: {
+      //         importLoaders: 1,
+      //         modules: true,
+      //         localIdentName: '[name]__[local]___[hash:base64:5]',
+      //         sourceMap: true
+      //       }
+      //     }
+      //   ]
+      // }
     ]
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Production'
-    }),
-    new MiniCssExtractPlugin()
+    })
+    // new MiniCssExtractPlugin({
+    //   filename: '[name].css',
+    // })
   ]
 }
 
